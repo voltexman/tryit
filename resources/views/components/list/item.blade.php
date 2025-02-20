b13a4e25420538f2fa280e9dcc3b79304a6a1232
voltexman <voltexman@gmail.com>
2025-02-20 14:13:12 +0200

    styled

--- resources/views/components/list/item.blade.php
+++ resources/views/components/list/item.blade.php
@@ -1,13 +1,13 @@
 @props(['index' => null])
 
-<li class="flex items-center gap-x-2.5 uppercase py-2.5 border-b border-dashed border-tryit-orange/50 last:border-0">
-    <span class="bg-tryit-orange/20 size-8 text-sm rounded-full flex-none flex items-center justify-center">
-        <x-lucide-check class="size-5 hidden flex-none text-tryit-orange lg:group-data-[type=check]:block" />
+<li class="flex items-center gap-x-2.5 py-2.5 border-b border-dashed border-tryit-orange/50 last:border-0">
+    <span class="hidden bg-tryit-orange/20 size-8 text-sm rounded-full flex-none lg:flex items-center justify-center">
+        <x-lucide-check class="size-5 flex-none text-tryit-orange lg:group-data-[type=check]:block" />
         @isset($index)
-            <span class="font-semibold text-sm text-tryit-orange">{{ $index }}</span>
+            <span class="hidden lg:block font-semibold text-sm text-tryit-orange">{{ $index }}</span>
         @endisset
     </span>
-    <span class="">
+    <span class="text-sm uppercase">
         {{ $slot }}
     </span>
 </li>
